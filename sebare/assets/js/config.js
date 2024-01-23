b86fbb06bbfd07d3788432848651b3923e41a200
Sebare81 <125512389+Sebare81@users.noreply.github.com>
2024-01-23 23:26:39 +0100

    Update config.js

--- sebare/assets/js/config.js
+++ sebare/assets/js/config.js
@@ -1,5 +1,5 @@
 const config = {
-      url: 'https://xatme.com/api/me/Sebare',
+      url: 'https://xatme.com/api/v2/me/sebare',
       xat: 'https://xat.com/MafiaGryzonia',
       color: '#495057'
 }
