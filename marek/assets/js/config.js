1fb4ee1b5071ed6528a259119389d0028a2d9692
Sebare81 <125512389+Sebare81@users.noreply.github.com>
2024-01-23 23:38:21 +0100

    Update config.js

--- marek/assets/js/config.js
+++ marek/assets/js/config.js
@@ -1,5 +1,5 @@
 const config = {
-      url: 'https://xatme.com/api/me/Marek (2018)',
+      url: 'https://xatme.com/api/v2/me/marek',
       xat: 'https://xat.com/MafiaGryzonia',
       color: '#00cc33'
 }
